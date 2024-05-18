fb394842c24a36d8de623a556bd7ca7232982af1
Your Name <you@example.com>
2024-05-18 16:49:11 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -140,7 +140,7 @@ Node *DFS(Node *initial, int *cont) {
     }
     List *list = get_adj_nodes(n);
     Node *aux = first(list);
-    while (aux != NULL) {
+    while (aux) {
       push(S, aux);
       aux = next(list);
     }
